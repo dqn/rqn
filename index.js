cbfa1afac546a213b27f04b1c70c0bc8a447ea40
dqn <suyarizzz@gmail.com>
2020-01-04 04:29:03 +0900

    feat: support https

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 'use strict';
 
 const net = require('net');
+const tls = require('tls');
 const { buildRequestMessage, parseResponseMessage } = require('./lib/util');
+
+function isSecure(protocol) {
+  switch (protocol) {
+    case 'http:':
+      return false;
+    case 'https:':
+      return true;
+    default:
+      throw new Error(`Invalid protocol: ${protocol}`);
+  }
+}
+
+function makeSocketConfig(url) {
+  let socketModule = null;
+  const socketOptions = { host: url.hostname };
+
+  if (isSecure(url.protocol)) {
+    socketModule = tls;
+    socketOptions.port = url.port || 443;
+    socketOptions.rejectUnauthorized = false;
+  } else {
+    socketModule = net;
+    socketOptions.port = url.port || 80;
+  }
+
+  return { socketModule, socketOptions };
+}
 
 function request(method, uri, options) {
   const url = new URL(uri);
+  const { socketModule, socketOptions } = makeSocketConfig(url);
   const message = buildRequestMessage(method, url, options);
 
-  const client = net.connect(url.port || 80, url.hostname, () => {
+  const client = socketModule.connect(socketOptions, () => {
     client.write(message);
   });
 
