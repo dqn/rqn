c1d31b26e60e69c72c0d57c6c55681bc34d1d729
dqn <suyarizzz@gmail.com>
2020-07-24 01:15:12 +0900

    refactor: modify style

--- src/rqn.ts
+++ src/rqn.ts
@@ -4,6 +4,17 @@ import tls from 'tls';
 const CRLF = '\r\n';
 const HTTP_VERSION = 1.1;
 
+export type HttpMethod =
+  | 'GET'
+  | 'HEAD'
+  | 'POST'
+  | 'PUT'
+  | 'DELETE'
+  | 'CONNECT'
+  | 'OPTIONS'
+  | 'TRACE'
+  | 'PATCH';
+
 export type RequestParamValue =
   | { [key: string]: RequestParamValue }
   | RequestParamValue[]
@@ -30,14 +41,6 @@ export type RqnResponse = {
 
 export type Headers = { [key: string]: string };
 
-function splitOnce(str: string, splitter: string): string[] {
-  const indexToSplit = str.indexOf(splitter);
-  const first = str.slice(0, indexToSplit);
-  const second = str.slice(indexToSplit + splitter.length);
-
-  return [first, second];
-}
-
 function stringifyQueryParams(obj: RequestParam): string {
   return Object.entries(obj)
     .map(([key, value]) => `${key}=${encodeURIComponent(value?.toString() ?? 'null')}`)
@@ -50,18 +53,7 @@ function stringifyHeaders(obj: Headers): string {
     .join(CRLF);
 }
 
-function parseHeaders(str: string): Headers {
-  return str
-    .trim()
-    .split(CRLF)
-    .reduce<Headers>((headers, row) => {
-      const [key, value] = splitOnce(row, ':');
-      headers[key] = value.trimLeft();
-      return headers;
-    }, {});
-}
-
-function buildRequestMessage(method: string, url: URL, options?: RqnRequestOptions): string {
+function buildRequestMessage(method: HttpMethod, url: URL, options?: RqnRequestOptions): string {
   options = options ?? {};
 
   const headers: Headers = { Host: url.hostname };
@@ -92,6 +84,29 @@ function buildRequestMessage(method: string, url: URL, options?: RqnRequestOptio
   return [startLine, stringifiedHeaders, '', body].join(CRLF);
 }
 
+function splitOnce(str: string, splitter: string): string[] {
+  const indexToSplit = str.indexOf(splitter);
+  const first = str.slice(0, indexToSplit);
+  const second = str.slice(indexToSplit + splitter.length);
+
+  return [first, second];
+}
+
+function parseHeaders(str: string): Headers {
+  return str
+    .trim()
+    .split(CRLF)
+    .reduce<Headers>((headers, row) => {
+      const [key, value] = splitOnce(row, ':');
+      headers[key] = value.trimLeft();
+      return headers;
+    }, {});
+}
+
+function sliceTextByByte(text: string, byte: number): string {
+  return Buffer.from(text).slice(0, byte).toString();
+}
+
 function parseInitialResponse(data: string): RqnResponse {
   const [statusLineAndHeaders, body] = splitOnce(data, CRLF + CRLF);
   const [statusLine, headers] = splitOnce(statusLineAndHeaders, CRLF);
@@ -100,51 +115,23 @@ function parseInitialResponse(data: string): RqnResponse {
   return { statusCode: Number(statusCode), headers: parseHeaders(headers), body };
 }
 
-function sliceTextByByte(text: string, byte: number): string {
-  return Buffer.from(text).slice(0, byte).toString();
-}
-
-function connect(url: URL, connectListener: () => void): net.Socket {
-  switch (url.protocol) {
-    case 'http:': {
-      const options = {
-        host: url.hostname,
-        port: Number(url.port) || 80,
-      };
-
-      return net.connect(options, connectListener).setEncoding('utf8');
-    }
-    case 'https:': {
-      const options = {
-        host: url.hostname,
-        port: Number(url.port) || 443,
-        rejectUnauthorized: false,
-      };
-
-      return tls.connect(options, connectListener).setEncoding('utf8');
-    }
-
-    default:
-      throw new Error(`Invalid protocol: ${url.protocol}`);
-  }
-}
-
 function processResponse(socket: net.Socket): Promise<RqnResponse> {
   let response: RqnResponse | null = null;
   let remainingBytes = 0;
   let contentLength = 0;
-  let parser = (data: string) => {};
+  let parseData = (_data: string) => {};
 
-  const onData = (data: string) => {
+  const handleData = (data: string) => {
     if (!response) {
+      // for initial response
       response = parseInitialResponse(data);
 
       if (response.headers['Content-Length']) {
         contentLength = Number(response.headers['Content-Length']);
-        parser = parseUsingContentLength;
+        parseData = parseUsingContentLength;
       } else if (response.headers['Transfer-Encoding'] === 'chunked') {
         remainingBytes = 0;
-        parser = parseUsingChunked;
+        parseData = parseUsingChunked;
       } else {
         socket.end();
         return;
@@ -154,34 +141,42 @@ function processResponse(socket: net.Socket): Promise<RqnResponse> {
       response.body = '';
     }
 
-    parser(data);
+    parseData(data);
   };
 
   const parseUsingContentLength = (data: string) => {
-    response!.body += data;
+    if (!response) {
+      return;
+    }
 
-    if (contentLength === Buffer.byteLength(response!.body)) {
+    response.body += data;
+
+    if (contentLength === Buffer.byteLength(response.body)) {
       socket.end();
     }
   };
 
   const parseUsingChunked = (data: string) => {
+    if (!response) {
+      return;
+    }
+
     if (remainingBytes === 0) {
-      const splitted = splitOnce(data, CRLF);
-      remainingBytes = Number.parseInt(splitted[0], 16);
+      const [remainingBytesStr, nextData] = splitOnce(data, CRLF);
+      remainingBytes = Number.parseInt(remainingBytesStr, 16);
 
       if (remainingBytes === 0) {
         socket.end();
         return;
       }
 
-      data = splitted[1];
+      data = nextData;
     }
 
     const sliced = sliceTextByByte(data, remainingBytes);
     data = data.slice(sliced.length);
 
-    response!.body += sliced;
+    response.body += sliced;
     remainingBytes -= Buffer.byteLength(sliced);
 
     if (remainingBytes === 0) {
@@ -194,14 +189,38 @@ function processResponse(socket: net.Socket): Promise<RqnResponse> {
   };
 
   return new Promise((resolve, reject) => {
-    socket.on('data', onData);
-    socket.on('end', () => resolve(response!));
+    socket.on('data', (data: Buffer) => handleData(data.toString()));
+    socket.on('end', () => response && resolve(response));
     socket.on('error', reject);
   });
 }
 
-async function request(
-  method: string,
+function connect(url: URL, connectListener: () => void): net.Socket {
+  switch (url.protocol) {
+    case 'http:': {
+      const options = {
+        host: url.hostname,
+        port: Number(url.port) || 80,
+      };
+
+      return net.connect(options, connectListener);
+    }
+    case 'https:': {
+      const options = {
+        host: url.hostname,
+        port: Number(url.port) || 443,
+        rejectUnauthorized: false,
+      };
+
+      return tls.connect(options, connectListener);
+    }
+    default:
+      throw new Error(`Invalid protocol: ${url.protocol}`);
+  }
+}
+
+export async function request(
+  method: HttpMethod,
   url: string,
   options?: RqnRequestOptions,
 ): Promise<RqnResponse> {
@@ -219,18 +238,20 @@ async function request(
   return response;
 }
 
-export function get(url: string, options?: RqnRequestOptions) {
+function _get(url: string, options?: RqnRequestOptions) {
   return request('GET', url, options);
 }
 
-export function post(url: string, options?: RqnRequestOptions) {
+function _post(url: string, options?: RqnRequestOptions) {
   return request('POST', url, options);
 }
 
-export function put(url: string, options?: RqnRequestOptions) {
+function _put(url: string, options?: RqnRequestOptions) {
   return request('PUT', url, options);
 }
 
-export function _delete(url: string, options?: RqnRequestOptions) {
+function _delete(url: string, options?: RqnRequestOptions) {
   return request('DELETE', url, options);
 }
+
+export { _get as get, _post as post, _put as put, _delete as delete };
